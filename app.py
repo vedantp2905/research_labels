4fe3512baf51675b362b14f64283c2fc39d881c5
vedantp2905 <vedantpungliya29@gmail.com>
2024-11-07 17:12:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -313,7 +313,7 @@ def main():
                 v1_label_data = comparator.v1_labels[current_cluster_key]
                 st.write("LLM Label (not specific to syntactic or semantic):")
                 st.write(v1_label_data.get("Labels", ["N/A"])[0])
-                st.write("Acceptability:", v1_label_data.get("Q1_Answer", "N/A"))
+                st.write("LLM label Acceptability:", v1_label_data.get("Q1_Answer", "N/A"))
                 
                 # Display Semantic one below the other
                 semantic_tags = v1_label_data.get("Semantic", "").split(", ")  # Split by comma for individual tags
